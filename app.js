1fc79b00647c111428cf5c7800209fb4e2b01cfd
Yoshihiro Saito <yshr446@gmail.com>
2021-05-01 21:17:16 +0900

    Add prettier

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const {App} = require('@slack/bolt');
-const {WebClient} = require('@slack/web-api');
+const { App } = require("@slack/bolt");
+const { WebClient } = require("@slack/web-api");
 
 const token = process.env.SLACK_BOT_TOKEN;
-const signingSecret = process.env.SLACK_SIGNING_SECRET
+const signingSecret = process.env.SLACK_SIGNING_SECRET;
 
 const web = new WebClient(token);
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token,
-  signingSecret
+  signingSecret,
 });
 
 function shuffle(arr) {
@@ -24,39 +24,39 @@ function shuffle(arr) {
   return arr;
 }
 
-app.command('/random', async ({command, ack, respond}) => {
+app.command("/random", async ({ command, ack, respond }) => {
   // コマンドリクエストを確認
   await ack();
 
-  if (command.text === '') {
+  if (command.text === "") {
     await respond(`/random [@グループ名] [数] の形式で入力してください！`);
-    return
+    return;
   }
 
-  const args = command.text.split(' ')
-  const groupName = args[0]
-  const takeSize = args[1] || 1
+  const args = command.text.split(" ");
+  const groupName = args[0];
+  const takeSize = args[1] || 1;
 
-  const groupId = groupName.replace('<!subteam^', '').split('|')[0];
-  const members = await web.usergroups.users.list({usergroup: groupId}).then(members => members.users).catch(_e => {
+  const groupId = groupName.replace("<!subteam^", "").split("|")[0];
+  const members = await web.usergroups.users
+    .list({ usergroup: groupId })
+    .then((members) => members.users)
+    .catch((_e) => {
       // noop
-    }
-  )
+    });
   if (members === undefined) {
-    await respond(`グループが見つかりませんでした`)
-    return
+    await respond(`グループが見つかりませんでした`);
+    return;
   }
   // takeSizeに応じてメンバーを取得
-  const selectedMembers = shuffle(members).slice(0, takeSize)
-  const message = selectedMembers.map(m => `\`<@${m}>\``).join(', ')
+  const selectedMembers = shuffle(members).slice(0, takeSize);
+  const message = selectedMembers.map((m) => `\`<@${m}>\``).join(", ");
 
-  await respond(
-    {response_type: "in_channel", text: message}
-  )
+  await respond({ response_type: "in_channel", text: message });
 });
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
