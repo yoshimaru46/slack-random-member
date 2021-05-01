44d13f77983859fe5e5deac5bf545fda82f8d5ad
Yoshihiro Saito <yshr446@gmail.com>
2021-05-01 23:18:16 +0900

    Fix to use module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { App } = require("@slack/bolt");
-const { WebClient } = require("@slack/web-api");
-const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+import { WebClient } from "@slack/web-api";
+import { App } from "@slack/bolt";
 
 /**
  * Returns the secret string from Google Cloud Secret Manager
